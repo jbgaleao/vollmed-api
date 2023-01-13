928ddc9b4642817a2f12d0b626919966f0998c21
cv7n <joao.galeao.SPASSU@petrobras.com.br>
2023-01-13 12:48:40 -0300

    Adicionada mensagem de erro em campos obrigatórios

--- api/src/main/java/med/voll/api/models/paciente/PacienteDTO.java
+++ api/src/main/java/med/voll/api/models/paciente/PacienteDTO.java
@@ -1,31 +1,29 @@
 package med.voll.api.models.paciente;
 
-import jakarta.persistence.Enumerated;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import med.voll.api.models.endereco.EnderecoDTO;
-import med.voll.api.models.enums.Especialidade;
 public record PacienteDTO(
 
         @NotBlank(message = "Campo obrigatório")
         String nome,
 
-        @NotBlank
+        @NotBlank(message = "Campo obrigatório")
         @Email
         String email,
 
-        @NotBlank
-        @Pattern(regexp = "\\d{10,11}")
+        @NotBlank(message = "Campo obrigatório")
+        @Pattern(regexp = "\\d{11}")
         String telefone,
 
-        @NotBlank
+        @NotBlank(message = "Campo obrigatório")
         @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}")
         String cpf,
 
-        @NotNull
+        @NotNull(message = "Campo obrigatório")
         @Valid
         EnderecoDTO endereco
 ) {
